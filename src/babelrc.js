a443008430b11a44fb63750ff73fdfc8e555bf34
Maxime Fabre <ehtnam6@gmail.com>
2016-01-08 14:39:28 +0100

    Unecessary quotes

--- src/babelrc.js
+++ src/babelrc.js
@@ -3,8 +3,8 @@ module.exports = {
     presets:        ['es2015', 'react', 'stage-0'],
     plugins:        ['transform-decorators-legacy'],
     env:            {
-        "development": {
-            "presets": ["react-hmre"]
+        development: {
+            presets: ["react-hmre"]
         },
         production:    {
             ast:     false,
