332d901036e57a601f19f76a372d0f2314531d5d
Maxime Fabre <ehtnam6@gmail.com>
2016-03-19 16:00:30 +0100

    Add naive tests
    Signed-off-by: Maxime Fabre <ehtnam6@gmail.com>

---
+++ tests/src/SomeComponent.js
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class SomeComponent extends Component {
+    static propTypes = {};
+
+    render() {
+        return (
+            <div>Foobar</div>
+        );
+    }
+}
---
+++ tests/webpack.config.js
@@ -0,0 +1,4 @@
+module.exports = require('../index')({
+    sourcePath: 'src',
+    outputPath: 'builds',
+});
