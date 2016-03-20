56ba210ee6c8c6b6bf49f83f3032effe1825bed3
Maxime Fabre <ehtnam6@gmail.com>
2016-03-20 23:12:53 +0100

    Revert "Add transform-runtime"
    This reverts commit 7300485b58430225c14421e5d981cb844bf8ec50.

--- src/loaders/babelrc.js
+++ src/loaders/babelrc.js
@@ -2,7 +2,7 @@ export default function (options) {
     return {
         cacheDirectory: true,
         presets: options.react ? ['es2015', 'react', 'stage-0'] : ['es2015', 'stage-0'],
-        plugins: ['transform-runtime', 'transform-decorators-legacy'],
+        plugins: ['transform-decorators-legacy'],
         env: {
             development: {
                 presets: (options.react && options.hot) ? ["react-hmre"] : [],
