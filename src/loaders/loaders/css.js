9890164b5592a381043056e8277671c81f5556f6
Maxime Fabre <ehtnam6@gmail.com>
2016-10-31 11:28:37 +0100

    Work on Webpack 2 support

--- src/loaders/loaders/css.js
+++ src/loaders/loaders/css.js
@@ -3,6 +3,6 @@ import ExtractText from 'extract-text-webpack-plugin';
 export default function (options) {
     return {
         test: /\.css$/,
-        loader: ExtractText.extract('style', options.loaders.css),
-    };
-}
+        loader: ExtractText.extract({fallbackLoader: 'style', loader: options.loaders.css}),
+    }
+};
--- src/loaders/loaders/fontgen.js
+++ src/loaders/loaders/fontgen.js
@@ -3,6 +3,6 @@ import ExtractText from 'extract-text-webpack-plugin';
 export default function (options) {
     return {
         test: /\.font\.json$/,
-        loader: ExtractText.extract('style', `${options.loaders.css}!fontgen?embed`),
+        loader: ExtractText.extract({fallbackLoader: 'style', loader: `${options.loaders.css}!fontgen?embed`}),
     };
 }
