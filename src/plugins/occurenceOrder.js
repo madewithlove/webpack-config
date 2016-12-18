68c1fd566189799c96e6620d1910e09b1a571a55
Maxime Fabre <ehtnam6@gmail.com>
2016-12-18 17:37:21 +0100

    Fix typo in plugin name

--- src/plugins/occurenceOrder.js
+++ src/plugins/occurenceOrder.js
@@ -1,5 +1,5 @@
 import webpack from 'webpack';
 
 export default function () {
-    return new webpack.optimize.OccurenceOrderPlugin(true);
+    return new webpack.optimize.OccurrenceOrderPlugin(true);
 }
